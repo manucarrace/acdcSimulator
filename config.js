06f5e0c767e637ff605efff81bb225a9e22945ea
Manuel Carracedo Sánchez <macs@mac-518332.local>
2018-02-15 18:16:00 +0100

    Some work aligned with PR comments

--- config.js
+++ config.js
@@ -1,6 +1,19 @@
 var config = {};
 
-config.GCMessage = 'ABCDGC';
-config.MCMessage = 'ABCDMC';
+config ={
+    server: {
+        host : '0.0.0.0',
+        listenPort : '4330'
+    },
+    dashboard: {
+        protocol : 'http',
+        ip : '127.0.0.1',
+        port : '8086',
+        path : '/configEntry'
+    },
+    tank: {
+        sapCode : '50035212'
+    }
+};
 
 module.exports = config;
